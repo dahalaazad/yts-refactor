962972d008d6435b60b52c2117b0360a77a692d3
dahalaazad <aazad.dahal@gmail.com>
2023-04-08 10:42:37 +0545

    feat: Add routes to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./index.css";
+import { ErrorPage, Root } from "@app/routes";
+import { Navbar } from "@app/components";
+import { BrowseMovies, FavMovies, Home } from "@app/pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/favourites",
+    element: <FavMovies />,
+    children: [
+      {
+        path: "favourites/:movieSlug",
+      },
+    ],
+  },
+  {
+    path: "/browse",
+    element: <BrowseMovies />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
